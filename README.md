335384168eed8761a03c8fde267c256ad34a76de
LimeSoda <office@limesoda.at>
2013-12-05 11:46:41 +0100

    Added n98-magerun command ls:env:configure:ess:m2epro:set-license-key (#16282).

---
+++ README.md
@@ -0,0 +1,44 @@
+LimeSoda Environment Configuration
+=====================
+Enables developers to modify Magento installations based on the given environment. 
+
+Facts
+-----
+- version: 0.0.1
+- extension key: LimeSoda_EnvironmentConfiguration
+
+Description
+-----------
+Enables developers to modify Magento installations based on the given environment.
+
+Requirements
+------------
+- PHP >= 5.3.0
+- Mage_Core
+- n98-magerun
+
+Compatibility
+-------------
+- Magento >= EE 1.13.0.2 (may also work on older version)
+
+Installation Instructions
+-------------------------
+1. Install the extension via modman.
+
+Uninstallation
+--------------
+1. Just like any other modman installed extension.
+
+Developer
+---------
+Matthias Zeis  
+[http://www.limesoda.com](http://www.limesoda.com)  
+[@mzeis](https://twitter.com/mzeis)
+
+License
+-------
+[OSL - Open Software Licence 3.0](http://opensource.org/licenses/osl-3.0.php)
+
+Copyright
+---------
+(c) 2013 LimeSoda Interactive Marketing GmbH
---
+++ lib/n98-magerun/modules/LimeSoda_EnvironmentConfiguration/src/LimeSoda/Environment/Configure/Ess/M2ePro/SetLicenseKey.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace LimeSoda\Environment\Configure\Ess\M2ePro;
+
+use N98\Magento\Command\AbstractMagentoCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SetLicenseKey extends AbstractMagentoCommand
+{
+    const EXTENSION_NAME = 'Ess_M2ePro';
+    
+    protected function configure()
+    {
+      $this
+          ->setName('ls:env:configure:ess:m2epro:set-license-key')
+          ->addArgument('key', InputArgument::REQUIRED, 'Ess_M2ePro license key')
+          ->setDescription('Set the Ess_M2ePro license key')
+          ->setHelp(
+              <<<EOT
+              Sets the license key for Ess_M2ePro. The extension has to be installed and enabled.
+EOT
+              );
+          
+      ;
+    }
+
+   /**
+    * @param \Symfony\Component\Console\Input\InputInterface $input
+    * @param \Symfony\Component\Console\Output\OutputInterface $output
+    * @return int|void
+    */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output);
+        if ($this->initMagento()) {
+           if (\Mage::helper('core')->isModuleEnabled(self::EXTENSION_NAME) === false) {
+               $output->writeln("Extension '" . self::EXTENSION_NAME ."' is not installed.");
+               return;
+           } 
+            
+           $key = strip_tags($input->getArgument('key'));
+           
+           \Mage::helper('M2ePro/Primary')->getConfig()->setGroupValue(
+                '/'.\Mage::helper('M2ePro/Module')->getName().'/license/','key',(string)$key
+            );
+            //--------------------
+
+            \Mage::getModel('M2ePro/Servicing_Dispatcher')->processTasks(array(
+                \Mage::getModel('M2ePro/Servicing_Task_License')->getPublicNick()
+            ));
+            
+            \Mage::helper('M2ePro/data_cache')->removeValue('M2ePro/Config_Primary_data');
+           
+           $output->writeln("Set license key '" . $key . "'."); 
+        }
+    }
+} 
